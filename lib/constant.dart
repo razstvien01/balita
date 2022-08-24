625063025e8334f6698116fbe2afb41b8e34256f
razstvien01 <nicolenevanz.aricayos@gmail.com>
2022-08-24 21:55:34 +0800

    main.dart code

---
+++ lib/constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFFF8084);
+const kAccentColor = Color(0xFFF1F1F1);
+const kWhiteColor = Color(0xFFFFFFFF);
+const kLightColor = Color(0xFF808080);
+const kDarkColor = Color(0xFF303030);
+const kTransparent = Colors.transparent;
